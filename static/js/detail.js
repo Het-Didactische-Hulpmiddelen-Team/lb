2015ef6e6c168a56f7e29ceaad57da3993441f9d
lem-onade <taelemansst@gmail.com>
2020-02-24 15:31:56 +0100

    collapse items

--- static/js/detail.js
+++ static/js/detail.js
@@ -18,4 +18,21 @@ $(function() {
   function percentageToDegrees(percentage) {
     return percentage / 100 * 360
   }
+
+  // accordion for test list
+  $('.toggle').click(function(e) {
+  	e.preventDefault();
+
+    var $this = $(this);
+
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+    } else {
+        $this.parent().parent().find('li .inner').removeClass('show');
+        $this.parent().parent().find('li .inner').slideUp(350);
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+    }
+  });
 });
